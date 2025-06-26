f8911482c62df0f9e6504a128468e98410e12815
Sajal-kanwal <sajal.kanwal02@gmail.com>
2025-06-26 13:16:57 +0530

    Spendora Ready

--- prisma/migrations/20250623061223_init_schema/migration.sql
+++ prisma/migrations/20250623061223_init_schema/migration.sql
@@ -1,53 +1,53 @@
 -- CreateTable
 CREATE TABLE "UserSettings" (
-    "userId" TEXT NOT NULL PRIMARY KEY,
-    "currency" TEXT NOT NULL
+                                "userId" TEXT NOT NULL PRIMARY KEY,
+                                "currency" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Category" (
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "name" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "icon" TEXT NOT NULL,
-    "type" TEXT NOT NULL DEFAULT 'income'
+                            "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                            "name" TEXT NOT NULL,
+                            "userId" TEXT NOT NULL,
+                            "icon" TEXT NOT NULL,
+                            "type" TEXT NOT NULL DEFAULT 'income'
 );
 
 -- CreateTable
 CREATE TABLE "Transaction" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "upadateAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "amount" REAL NOT NULL,
-    "description" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "userId" TEXT NOT NULL,
-    "type" TEXT NOT NULL DEFAULT 'income',
-    "category" TEXT NOT NULL,
-    "categoryIcon" TEXT NOT NULL
+                               "id" TEXT NOT NULL PRIMARY KEY,
+                               "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               "upadateAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                               "amount" REAL NOT NULL,
+                               "description" TEXT NOT NULL,
+                               "date" DATETIME NOT NULL,
+                               "userId" TEXT NOT NULL,
+                               "type" TEXT NOT NULL DEFAULT 'income',
+                               "category" TEXT NOT NULL,
+                               "categoryIcon" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "MonthHistory" (
-    "userId" TEXT NOT NULL,
-    "day" INTEGER NOT NULL,
-    "month" INTEGER NOT NULL,
-    "year" INTEGER NOT NULL,
-    "income" REAL NOT NULL,
-    "expense" REAL NOT NULL,
+                                "userId" TEXT NOT NULL,
+                                "day" INTEGER NOT NULL,
+                                "month" INTEGER NOT NULL,
+                                "year" INTEGER NOT NULL,
+                                "income" REAL NOT NULL,
+                                "expense" REAL NOT NULL,
 
-    PRIMARY KEY ("day", "month", "year", "userId")
+                                PRIMARY KEY ("day", "month", "year", "userId")
 );
 
 -- CreateTable
 CREATE TABLE "YearHistory" (
-    "userId" TEXT NOT NULL,
-    "month" INTEGER NOT NULL,
-    "year" INTEGER NOT NULL,
-    "income" REAL NOT NULL,
-    "expense" REAL NOT NULL,
+                               "userId" TEXT NOT NULL,
+                               "month" INTEGER NOT NULL,
+                               "year" INTEGER NOT NULL,
+                               "income" REAL NOT NULL,
+                               "expense" REAL NOT NULL,
 
-    PRIMARY KEY ("month", "year", "userId")
+                               PRIMARY KEY ("month", "year", "userId")
 );
 
 -- CreateIndex
